d1a602cbc68ae0172fc050649e7cd0f66ac22d21
Mário Ponte <ponteolavo30@gmail.com>
2024-09-15 20:22:31 +0100

    adding draw for a specific team

--- app/page.tsx
+++ app/page.tsx
@@ -5,43 +5,34 @@ import { teams } from "./teams";
 import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
-function sortOpponents(pots: any) {
-  const matches:any = []; // Array para armazenar os confrontos
-  const teamsPerPot = pots.flat(); // Juntar todos os potes numa única lista de equipas
-  
-  teamsPerPot.forEach((team: any) => {
-    const opponents = [];
-    const usedTeams = new Set(); // Para evitar equipas repetidas ou do mesmo país
+function sortOpponents(team: any, pots: any) {
+  const teamMatches: any = { team: team.name, opponents: [] };
+  const selectedCountries: { [key: string]: number } = {};
 
-    for (let i = 0; i < pots.length; i++) {
-      // Selecionar 2 adversários de cada pote (exceto do pote da própria equipa)
-      const availableTeams = pots[i].filter(
-        (opponent: any) => 
-          opponent.id !== team.id && // Evitar que a equipa jogue contra si própria
-          opponent.country !== team.country && // Evitar equipas do mesmo país
-          !usedTeams.has(opponent.id) // Evitar equipas repetidas
-      );
+  // Função auxiliar para verificar se um clube pode ser selecionado
+  const canSelectTeam = (opponent: any) => team.id !== opponent.id && opponent.country !== team.country && (selectedCountries[opponent.country] || 0) < 2;
 
-      // Randomizar e escolher 2 equipas disponíveis do pote atual
-      while (opponents.length < (i + 1) * 2 && availableTeams.length > 0) {
-        const randomIndex = Math.floor(Math.random() * availableTeams.length);
-        const chosenOpponent = availableTeams.splice(randomIndex, 1)[0]; // Retira equipa do array
-        opponents.push(chosenOpponent);
-        usedTeams.add(chosenOpponent.id); // Marca equipa como já usada
-      }
-    }
-
-    // Armazenar os confrontos dessa equipa
-    matches.push({
-      team: team.name,
-      opponents: opponents.map((opponent: any) => opponent.name),
+  // Itera sobre cada pote para sortear 2 times
+  pots.forEach((pot: any) => {
+    const potOpponents = pot.filter(canSelectTeam).sort(() => 0.5 - Math.random());
+    potOpponents.slice(0, 2).forEach((opponent: any) => {
+      teamMatches.opponents.push(opponent);
+      selectedCountries[opponent.country] = (selectedCountries[opponent.country] || 0) + 1;
     });
   });
 
-  // Exibir os confrontos no console para revisão
-  console.log(matches);
-  return matches;
+  console.log(teamMatches);
+
+  return teamMatches;
 }
 
 export default function Home() {
@@ -50,8 +41,6 @@ export default function Home() {
   const pot2 = [teams[9], teams[10], teams[11], teams[12], teams[13], teams[14], teams[15], teams[16], teams[17]];
   const pot3 = [teams[18], teams[19], teams[20], teams[21], teams[22], teams[23], teams[24], teams[25], teams[26]];
   const pot4 = [teams[27], teams[28], teams[29], teams[30], teams[31], teams[32], teams[33], teams[34], teams[35]];
-
-  const draw = sortOpponents([pot1, pot2, pot3, pot4]);
 
   return (
     <Container>
@@ -63,7 +52,20 @@ export default function Home() {
         <Pot num={4} teams={pot4} />
       </div>
       <div className="text-center flex flex-col gap-8 items-center">
-        <Button className="w-fit bg-white text-[#010056] hover:bg-white/95 gap-1" onClick={() => draw}>
+        <Select>
+          <SelectTrigger className="bg-[#0a0a61] max-w-96 border-white/50">
+            <SelectValue placeholder="Select one club" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {teams.map(team => (
+                <SelectItem key={team.id} value={team.name}>{team.name}</SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Button className="bg-white text-[#010056] hover:bg-white/95 gap-1" onClick={() => sortOpponents(teams[19], [pot1, pot2, pot3, pot4])}>
           <Image width={100} height={100} className="h-8 w-8" alt="ucl icon" src="/images/uclLogo.svg" />
           <span>Draw Opponents</span>
         </Button>
