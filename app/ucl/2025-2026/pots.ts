888bc8c30a40af0a5c3b82ae1fcb9c43fc5d0b43
Mário Ponte <ponteolavo30@gmail.com>
2025-08-06 23:00:21 +0100

    starting the plans for the season 2025 2026

---
+++ app/ucl/2025-2026/pots.ts
@@ -0,0 +1,46 @@
+export const pots = [
+    [
+        { "id": 0, "name": "Paris Saint-Germain", "country": "fr", },
+        { "id": 1, "name": "Real Madrid", "country": "es", },
+        { "id": 2, "name": "Man City", "country": "gb-eng", },
+        { "id": 3, "name": "Bayern München", "country": "de", },
+        { "id": 4, "name": "Liverpool", "country": "gb-eng", },
+        { "id": 5, "name": "Internazionale Milano", "country": "it", },
+        { "id": 6, "name": "Chelsea", "country": "gb-eng", },
+        { "id": 7, "name": "Borussia Dortmund", "country": "de", },
+        { "id": 8, "name": "FC Barcelona", "country": "es", }
+    ],
+    [
+        { "id": 9, "name": "Arsenal", "country": "gb-eng", },
+        { "id": 10, "name": "Bayer Leverkusen", "country": "de", },
+        { "id": 11, "name": "Atlético de Madrid", "country": "es", },
+        { "id": 12, "name": "Atalanta", "country": "it", },
+        { "id": 13, "name": "Villarreal", "country": "es", },
+        { "id": 14, "name": "Juventus", "country": "it", },
+        { "id": 15, "name": "Eintracht Frankfurt", "country": "de", },
+        { "id": 16, "name": "Shakhtar Donetsk", "country": "ua", },
+        { "id": 17, "name": "AC Milan", "country": "it", }
+    ],
+    [
+        { "id": 18, "name": "Ajax", "country": "nl", },
+        { "id": 19, "name": "Napoli", "country": "it", },
+        { "id": 20, "name": "Sporting CP", "country": "pt", },
+        { "id": 21, "name": "Olympiacos", "country": "gr", },
+        { "id": 22, "name": "Slavia Praha", "country": "cz", },
+        { "id": 23, "name": "Marseille", "country": "fr", },
+        { "id": 24, "name": "LOSC Lille", "country": "fr", },
+        { "id": 25, "name": "Crvena Zvezda", "country": "rs", },
+        { "id": 26, "name": "Young Boys", "country": "ch", },
+    ],
+    [
+        { "id": 27, "name": "Union Saint-Gilloise", "country": "be", },
+        { "id": 28, "name": "Athletic Club", "country": "es", },
+        { "id": 29, "name": "New Castle", "country": "gb-eng", }, 
+        { "id": 30, "name": "Aston Villa", "country": "gb-eng", },
+        { "id": 31, "name": "Bologna", "country": "it", },
+        { "id": 32, "name": "Girona FC", "country": "es", },
+        { "id": 33, "name": "Stuttgart", "country": "de", },
+        { "id": 34, "name": "Sturm Graz", "country": "at", },
+        { "id": 35, "name": "Stade Brestois", "country": "fr", },
+    ]
+]
