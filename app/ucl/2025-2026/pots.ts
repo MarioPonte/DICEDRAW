7b77d857fd273ae93562a8c676a1de5eb4769ded
Mário Ponte <ponteolavo30@gmail.com>
2025-08-28 12:34:23 +0100

    finishing ucl 25 26 data

--- app/ucl/2025-2026/pots.ts
+++ app/ucl/2025-2026/pots.ts
@@ -14,33 +14,33 @@ export const pots = [
         { "id": 9, "name": "Arsenal", "country": "gb-eng", },
         { "id": 10, "name": "Bayer Leverkusen", "country": "de", },
         { "id": 11, "name": "Atlético de Madrid", "country": "es", },
-        { "id": 12, "name": "Atalanta", "country": "it", },
-        { "id": 13, "name": "Villarreal", "country": "es", },
-        { "id": 14, "name": "Juventus", "country": "it", },
-        { "id": 15, "name": "Eintracht Frankfurt", "country": "de", },
-        { "id": 16, "name": "Shakhtar Donetsk", "country": "ua", },
-        { "id": 17, "name": "AC Milan", "country": "it", }
+        { "id": 12, "name": "SL Benfica", "country": "pt", },
+        { "id": 13, "name": "Atalanta", "country": "it", },
+        { "id": 14, "name": "Villarreal", "country": "es", },
+        { "id": 15, "name": "Juventus", "country": "it", },
+        { "id": 16, "name": "Eintracht Frankfurt", "country": "de", },
+        { "id": 17, "name": "Club Brugge", "country": "be", }
     ],
     [
-        { "id": 18, "name": "PSV Eindhoven", "country": "nl", },
-        { "id": 19, "name": "Ajax", "country": "nl", },
-        { "id": 20, "name": "Napoli", "country": "it", },
-        { "id": 21, "name": "Sporting CP", "country": "pt", },
-        { "id": 22, "name": "Olympiacos", "country": "gr", },
-        { "id": 23, "name": "Slavia Praha", "country": "cz", },
-        { "id": 24, "name": "Bodø/Glimt", "country": "no", },
-        { "id": 25, "name": "Marseille", "country": "fr", },
-        { "id": 26, "name": "Crvena Zvezda", "country": "rs", }
+        { "id": 18, "name": "Tottenham", "country": "gb-eng", },
+        { "id": 19, "name": "PSV Eindhoven", "country": "nl", },
+        { "id": 20, "name": "Ajax", "country": "nl", },
+        { "id": 21, "name": "Napoli", "country": "it", },
+        { "id": 22, "name": "Sporting CP", "country": "pt", },
+        { "id": 23, "name": "Olympiacos", "country": "gr", },
+        { "id": 24, "name": "Slavia Praha", "country": "cz", },
+        { "id": 25, "name": "Bodø/Glimt", "country": "no", },
+        { "id": 26, "name": "Marseille", "country": "fr", }
     ],
     [
-        { "id": 27, "name": "Monaco", "country": "fr", },
-        { "id": 28, "name": "Galatasaray", "country": "tr", },
-        { "id": 29, "name": "Union Saint-Gilloise", "country": "be", },
-        { "id": 30, "name": "Qarabağ", "country": "az", },
-        { "id": 31, "name": "Athletic Club", "country": "es", },
-        { "id": 32, "name": "New Castle", "country": "gb-eng", },
-        { "id": 33, "name": "Pafos", "country": "cy", },
-        { "id": 34, "name": "Kairat", "country": "kz", },
-        { "id": 35, "name": "Stuttgart", "country": "de", }
+        { "id": 27, "name": "København", "country": "dk", },
+        { "id": 28, "name": "Monaco", "country": "fr", },
+        { "id": 29, "name": "Galatasaray", "country": "tr", },
+        { "id": 30, "name": "Union Saint-Gilloise", "country": "be", },
+        { "id": 31, "name": "Qarabağ", "country": "az", },
+        { "id": 32, "name": "Athletic Club", "country": "es", },
+        { "id": 33, "name": "New Castle", "country": "gb-eng", },
+        { "id": 34, "name": "Pafos", "country": "cy", },
+        { "id": 35, "name": "Kairat", "country": "kz", }
     ]
 ]
